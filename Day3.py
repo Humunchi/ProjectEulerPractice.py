708ff33ff9630052e2a34bd81d4342edb053ec08
Humunchi <50121719+Humunchi@users.noreply.github.com>
2021-03-07 11:43:44 +0200

    Create Day3.py

---
+++ Day3.py
@@ -0,0 +1,3 @@
+#What is the largest prime factor of the number 600851475143 ?
+#The prime factors of 13195 are 5, 7, 13 and 29. (5*7*13*29=13195 and 5, 7, 13 and 29 are all prime numbers because they are only divisible by 1 and itself)
+#Prime Factor Tree 
